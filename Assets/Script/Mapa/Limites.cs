4889182c54d2d0f4730be46a8c9e87d496732043
ItreZez <okmecallo@hotmail.com>
2022-11-24 11:32:44 -0600

    1

--- Assets/Script/Mapa/Limites.cs
+++ Assets/Script/Mapa/Limites.cs
@@ -6,21 +6,37 @@ public class Limites : MonoBehaviour
 {
     [SerializeField] private bool IsTrigger;
     [SerializeField] private GameObject LimiteTutorial;
+    [SerializeField] private bool Escondido;
 
     void Start()
     {
         IsTrigger = LimiteTutorial.GetComponent<BoxCollider>().isTrigger;
+
+        Escondido = GameObject.Find("Player").GetComponent<FP_Controller>().Escondido;
     }
     void Update()
     {
         LimiteTutorial.GetComponent<BoxCollider>().isTrigger = IsTrigger;
+
+        GameObject.Find("Player").GetComponent<FP_Controller>().Escondido = Escondido;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Limit_Casa")
+        {
+            Escondido = true;
+        }
+    }
     private void OnTriggerExit(Collider other)
     {
         if(other.tag == "Limit_Tutorial")
         {
             IsTrigger = false;
+        }
+        if (other.tag == "Limit_Casa")
+        {
+            Escondido = false;
         }
     }
 }
