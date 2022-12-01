b530191edc1596b9832af4b7cc3fde1b73a9481b
ItreZez <okmecallo@hotmail.com>
2022-12-01 02:51:40 -0600

    menus

--- Assets/Script/Menu/MenuActive.cs
+++ Assets/Script/Menu/MenuActive.cs
@@ -6,11 +6,13 @@ public class MenuActive : MonoBehaviour
 {
     [SerializeField] private GameObject MainMenuCanvas;
     [SerializeField] private GameObject SettingsCanvas;
-    
+    [SerializeField] private GameObject CreditsCanvas;
     void Start()
     {
         MainMenuCanvas.SetActive(true);
         SettingsCanvas.SetActive(false);
+        CreditsCanvas.SetActive(false);
+
     }
 
     // Update is called once per frame
