e881d04c3e4ab9213b9b85e9ef694acac0bfa719
Jrrioss <jrrioss@live.com.mx>
2022-12-01 19:24:19 -0600

    Al fifn

--- Assets/Script/Enemy/Spawns/ContadorArañas.cs
+++ Assets/Script/Enemy/Spawns/ContadorArañas.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class ContadorArañas : MonoBehaviour
 {
     public int arañasEnMapa ;
+    public GameObject spawn1;
+    public GameObject spawn2;
+    public GameObject spawn3;
+    public GameObject spawn4;
+
+    public FP_Controller contenedor_De_Llaves;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +20,13 @@ public class ContadorArañas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(contenedor_De_Llaves.contadorLlaves == 5)
+        {
+            Destroy(spawn1);
+            Destroy(spawn2);
+            Destroy(spawn3);
+            Destroy(spawn4);
+        }
     }
 
    IEnumerator ReiniciarContador()
